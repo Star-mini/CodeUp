4fb93bfdbcfc45488ce6af38969284d7776eefc5
Park <minisstar@naver.com>
2023-07-09 18:41:27 +0900

    6068까지

---
+++ 6068.py
@@ -0,0 +1,32 @@
+a=input()
+
+if a=="A" :
+    print("best!!!")
+else:
+    if a=="B" :
+        print("good!!") 
+    else:
+        if a=="C":
+            print("run!")
+        else:
+            if a=="D" :
+                print("slowly~")           
+            else:
+                print("what?")
+
+
+'''
+a = input()
+
+if a == "A":
+    print("best!!!")
+elif a == "B":
+    print("good!!")
+elif a == "C":
+    print("run!")
+elif a == "D":
+    print("slowly~")
+else:
+    print("what?")
+
+'''
