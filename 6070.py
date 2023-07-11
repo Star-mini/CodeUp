949c9295decd8c147129148497f2093fc6034e36
Park <minisstar@naver.com>
2023-07-11 16:06:09 +0900

    6070정답

--- 6070.py
+++ 6070.py
@@ -1,10 +1,10 @@
-a=input()
+a = input()
 
-if a == 12 or 1 or 2:
+if a == 12 or a == 1 or a == 2:
     print("winter")
-if a == 3 or 4 or 5:
+if a == 3 or a == 4 or a == 5:
     print("spring")
-if a == 6 or 7 or 8:
+if a == 6 or a == 7 or a == 8:
     print("summer")
-if a == 9 or 10 or 11:
-    print("winter")
+if a == 9 or a == 10 or a == 11:
+    print("fall")
