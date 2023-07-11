a7bfacd76fd3fc39351a2f1b638e8b2a979be3af
Park <minisstar@naver.com>
2023-07-11 16:04:46 +0900

    6070오답

---
+++ 6070.py
@@ -0,0 +1,10 @@
+a=input()
+
+if a == 12 or 1 or 2:
+    print("winter")
+if a == 3 or 4 or 5:
+    print("spring")
+if a == 6 or 7 or 8:
+    print("summer")
+if a == 9 or 10 or 11:
+    print("winter")
