134292d8a9bccd898007dd8c2f44d1898fdaa3a0
Park <minisstar@naver.com>
2023-07-12 18:08:43 +0900

    6071까지

--- 6070.py
+++ 6070.py
@@ -1,4 +1,4 @@
-a = input()
+a = int(input())
 
 if a == 12 or a == 1 or a == 2:
     print("winter")
@@ -8,3 +8,19 @@ if a == 6 or a == 7 or a == 8:
     print("summer")
 if a == 9 or a == 10 or a == 11:
     print("fall")
+
+
+'''
+오답
+a=input()
+
+if a == 12 or 1 or 2:
+    print("winter")
+if a == 3 or 4 or 5:
+    print("spring")
+if a == 6 or 7 or 8:
+    print("summer")
+if a == 9 or 10 or 11:
+    print("winter")
+
+'''
---
+++ 6071.py
@@ -0,0 +1,8 @@
+a=input()
+
+a=int(a)
+
+while a !=0:
+    print(a)
+    a=input()
+    a=int(a)
