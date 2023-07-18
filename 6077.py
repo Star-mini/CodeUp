933b7b15cc18c100a1db38f925341efb51578f66
Park <minisstar@naver.com>
2023-07-19 03:52:23 +0900

    6078까지

---
+++ 6077.py
@@ -0,0 +1,10 @@
+a=input()
+
+a=int(a)
+
+s=0
+
+for i in range(a+1):
+    if i % 2== 0 :
+        s=s+i
+print(s)
---
+++ 6078.py
@@ -0,0 +1,15 @@
+a=input()
+
+while a != "q":
+    print(a)
+    a=input()
+print("q")
+
+'''
+while True:
+    a = input()
+    if a == 'q':
+        print(a)
+        break
+    print(a)
+'''
