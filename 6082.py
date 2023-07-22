7b801f7bb42afaab881a1bd2b9f8b1b11f978321
Park <minisstar@naver.com>
2023-07-22 18:45:31 +0900

    6082까지

---
+++ 6082.py
@@ -0,0 +1,15 @@
+a=input()
+
+a=int(a)
+
+i=0
+
+for i in range(1,a+1):
+    if (i % 10) ==3 :
+        print("X")
+    elif (i % 10 )== 6:
+        print("X")
+    elif (i % 10 )== 9:
+        print("X")
+    else:
+        print(i)
