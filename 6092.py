4085da01b9df23b3dbefa56a7b8c0e03eab2f1b0
Park <minisstar@naver.com>
2023-08-03 18:52:04 +0900

    6092수정

--- 6092.py
+++ 6092.py
@@ -1,4 +1,4 @@
-'''
+
 a=input()
 
 b=input().split()
@@ -7,7 +7,31 @@ a=int(a)
 
 for i in range(a):
     b[i]=int(b[i])
-'''
-c=[0]*24
 
-print(c)
+c=[0]*25
+
+for i in range(1,25):
+    for j in range(a):
+        if b[j]==i :
+            c[i]=c[i]+1
+
+
+for i in range(1,24):
+    print(c[i],end=' ')
+
+
+
+
+#내가 생각한 원리인데 그걸 컴퓨터가 연산할수있도록 코딩하자
+'''
+n = int(input())
+b = list(map(int, input().split()))
+
+c = [0]*24  # 24명의 학생에 대한 정보를 담을 수 있는 리스트를 생성
+
+for i in b:
+    c[i] += 1
+
+for i in range(1, 24):  # 1부터 23까지만 출력하도록 범위를 지정
+    print(c[i], end=' ')
+'''
